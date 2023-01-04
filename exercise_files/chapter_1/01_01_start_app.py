519a6fcbe49a71c241ecdb481b01bae1a6ddd2f6
isoldaliborio <isoldaliborio@gmail.com>
2023-01-04 13:28:45 +0000

    exercises installations

--- exercise_files/chapter_1/01_01_start_app.py
+++ exercise_files/chapter_1/01_01_start_app.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return jsonify(message = 'Hello World!')
 
 
 @app.route('/super_simple')
 def super_simple():
-    return "hello from the app api training"
+    return jsonify(message = "hello from the app api training")
+
+
+@app.route("/not_found")
+def not_found():
+    return jsonify(message = "That resource was not found"), 404
 
 
 if __name__ == '__main__':
