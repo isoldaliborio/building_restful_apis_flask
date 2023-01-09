8b3ca6a6ec6f70fb1f280b259b2edc9beee2e432
isoldaliborio <isoldaliborio@gmail.com>
2023-01-09 23:31:03 +0000

    traing how to work with database

--- exercise_files/chapter_1/01_01_start_app.py
+++ exercise_files/chapter_1/01_01_start_app.py
@@ -1,14 +1,72 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "planets.db")
 
 
-@app.route('/')
+db=SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+@app.cli.command("db_create")
+def db_create():
+    db.create_all()
+    print("Database created!")
+
+
+@app.cli.command("db_drop")
+def db_drop():
+    db.drop_all()
+    print("Database droped!")
+
+
+@app.cli.command("db_seed")
+def db_seed():
+    mercury = Planet(planet_name = "Mercury",
+    planet_type = "Class D",
+    home_star = "sol",
+    mass = 2.358e23,
+    radius = 1516,
+    distance = 3598e6)
+
+    venus = Planet(planet_name = "Venus",
+                   planet_type = "Class K",
+                   home_star = "sol",
+                   mass = 4.867e24,
+                   radius = 3760,
+                   distance = 67.24e6)
+
+    earth = Planet(planet_name = "Earth",
+                   planet_type = "Class M",
+                   home_star = "sol",
+                   mass = 5.972e24,
+                   radius = 3959,
+                   distance = 92.96e6)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(first_name = "Ada",
+                    last_name = "Lovelace",
+                    email = "teste@test.com",
+                    password = "1234")
+
+    db.session.add(test_user)
+    db.session.commit()
+    print("Database seeded!")
+
+@app.route("/")
 def hello_world():
     return jsonify(message = 'Hello World!')
 
 
-@app.route('/super_simple')
+@app.route("/super_simple")
 def super_simple():
     return jsonify(message = "hello from the app api training")
 
@@ -17,6 +75,66 @@ def super_simple():
 def not_found():
     return jsonify(message = "That resource was not found"), 404
 
+
+@app.route("/parameters")
+def parameters():
+    name = request.args.get("name")
+    age = int(request.args.get("age"))
+    if age < 18:
+        return jsonify(message = "Sorry " + name + ", you are not old enough"), 401 
+    else:
+        return jsonify(message = "wellcome " + name + ", you are old enough")
+
+
+@app.route("/url_variables/<string:name>/<int:age>")
+def url_variables(name:str, age:int):
+    if age < 18:
+        return jsonify(message = "Sorry " + name + ", you are not old enough"), 401 
+    else:
+        return jsonify(message = "wellcome " + name + ", you are old enough")
+
+
+@app.route("/planets", methods=["GET"])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+
+
+#database model 
+class User(db.Model):
+    tablename = "users"
+    id = Column(Integer, primary_key = True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique = True)
+    password = Column(String)
+
+class Planet(db.Model):
+    __tablename__ = "planets"
+    planet_id = Column(Integer, primary_key = True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+
+#Create a class to indicates  that tells Marshmallow what field is looking for.
+class UserSchema(ma.Schema):
+    class Meta: 
+        fields = ("id", "first_name","lastname", "email", "password")
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ("planet_id", "planet_name", "planet_type", "home_star", "mass", "radius", "distance")
+
+#defining the ability to deserialize a single object (as when we get one planet back later on we make the route for that, maybe for the details) x geting a collection of records back. 
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
 
 if __name__ == '__main__':
     app.run()
