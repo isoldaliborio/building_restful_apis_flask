b16afed509847d6a06ab5b61614ddb2d1b28355a
isoldaliborio <isoldaliborio@gmail.com>
2023-01-12 18:57:59 +0000

    API security training

--- exercise_files/chapter_1/01_01_start_app.py
+++ exercise_files/chapter_1/01_01_start_app.py
@@ -4,16 +4,21 @@ from sqlalchemy import Column, Integer, String, Float
 import os
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "planets.db")
 app.config["JWT_SECRET_KEY"] = "super-secret" #change this IRL
+app.config["MAIL_SERVER"] = "smpt.mailtrap.io"
+app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
+app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
 
 db=SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
+mail = Mail(app)
 
 
 @app.cli.command("db_create")
@@ -135,6 +140,23 @@ def login():
         return jsonify(message = "Login succeeded!", access_token=access_token)
     else:
         return jsonify(message = "Bad email or password."), 401
+
+
+@app.route("/retrieve_password/<string:email>", methods = ["GET"])
+def retrieve_password(email:str):
+    user = User.query.filter_by( email=email).first()
+    if user:
+        msg = Message("Your planetary API password is " + user.password, 
+                  sender="admin@planetary-api.com",
+                  recipients=[email])
+        mail.send(msg)
+        return jsonify(message = "Password sent to " +   email)
+    else:
+        return jsonify(message = "That email doesn't exist.")
+   
+
+
+            
 
 #database model 
 class User(db.Model):
